b6e48e940968721fc393236265f785c6f1202d40
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-04-16 22:21:11 -0500

    feat: implemented main content section

---
+++ src/components/news/components/main-content.component.vue
@@ -0,0 +1,21 @@
+<script setup>
+import ArticleCard from "@/components/news/components/article-card.component.vue";
+export default {
+  name: "main-content",
+  components: {ArticleCard},
+  props: {
+    articles: Array
+  }
+}
+
+</script>
+
+<template>
+  <div v-for="article in articles">
+    <article-card :article="article"></article-card>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
