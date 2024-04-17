b650f73bfb56601e08b9462b0b005027cab65aab
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-04-16 22:24:25 -0500

    feat: implemented unavailable content view

--- src/components/news/components/unavailable-content.component.vue
+++ src/components/news/components/unavailable-content.component.vue
@@ -1,10 +1,16 @@
 <script>
+export default {
+  name: "unavailable-content",
+  props: {
+    errors: null,
+  }
+}
 
 </script>
 
 <template>
   <div><h4>News Service is unavailable now</h4></div>
-  <div v-for="error in erros"><h6>{{ error }}</h6></div>
+  <div v-for="error in errors"><h6>{{ error }}</h6></div>
 </template>
 
 <style scoped>
