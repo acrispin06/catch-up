e769d8efaa5c11b9fb80e037d5091144c97676eb
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-04-16 22:46:18 -0500

    feat: main view

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,78 @@
-<script setup>
+<script>
+import {NewsApiService} from "@/components/news/service/news-api.service.js";
+import SideMenu from "@/components/news/components/side-menu.component.vue";
+import UnavailableContent from "@/components/news/components/unavailable-content.component.vue";
+import MainContent from "@/components/news/components/main-content.component.vue";
+import FooterContent from "@/components/news/components/footer-content.component.vue";
+
+export default{
+  name: "App",
+  component: {FooterContent, UnavailableContent, MainContent, SideMenu},
+  data(){
+    return {
+      articles: [],
+      errors: [],
+      newsApi: new NewsApiService(),
+      sidebarVisible: false
+    };
+  },
+  created() {
+    this.getArticlesForSource('bbc-news');
+  },
+  methods: {
+    //fetch articles for a given source
+    getArticlesForSource(sourceId) {
+      this.newsApi.getArticlesForSource(sourceId)
+        .then(response => {
+          this.articles = response.data.articles;
+          console.log(response.data.articles);
+        })
+        .catch(e => this.errors.push(e));
+    },
+    //fetch articles for selected source with logo url
+    getArticlesForSourceWithUrl(source){
+      this.newsApi.getArticlesForSource(source.id)
+        .then(response => {
+          this.articles = response.data.articles;
+          this.articles.map(article => article.source.urlToLogo = source.urlToLogo);
+          console.log(response.data.articles);
+        })
+        .catch(e => this.errors.push(e));
+    },
+    //on source selected
+    setSource(source){
+      this.getArticlesForSourceWithUrl(source);
+      this.toggleSidebar();
+    },
+    toggleSidebar(){
+      this.sidebarVisible = !this.sidebarVisible;
+    }
+  }
+}
+
+
 
 </script>
 
 <template>
-
+  <div class="w-full">
+    <div>
+      <pv-menubar class="sticky bg-primary">
+        <template #start>
+          <pv-button label="CatchUp" icon="pi pi-bars" @click="toggleSidebar"></pv-button>
+          <side-menu v-model:visible="sidebarVisible" v-on:source-selected="setSource"></side-menu>
+        </template>
+        <template #end>
+          <pv-button label="Sign In" icon="pi pi-user"></pv-button>
+        </template>
+      </pv-menubar>
+    </div>
+    <div>
+      <unavailable-content v-if="errors" :errors="errors"></unavailable-content>
+      <main-content v-else :articles="articles"></main-content>
+    </div>
+    <footer-content></footer-content>
+  </div>
 </template>
 
 <style scoped>
--- src/components/news/components/footer-content.component.vue
+++ src/components/news/components/footer-content.component.vue
@@ -5,11 +5,13 @@ export default {
 </script>
 
 <template>
-  <div>
-    <p>Copyright &copy; 2024 ALVARO CRISPIN DEV</p>
-  </div>
-  <div>
-    <p>Made with love using Vue</p>
+  <div class="grid bg-primary mt-4 p-4 align-content-start">
+    <div class="col-12 ml-3 align-items-center justify-content-center">
+      <p>Copyright &copy; 2024 ALVARO CRISPIN DEV</p>
+    </div>
+    <div class="col-12 ml-3 mt-3 align-items-center justify-content-center">
+      <p>Made with love using Vue</p>
+    </div>
   </div>
 </template>
 
