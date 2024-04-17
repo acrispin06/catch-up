a916b7f1e3b9db1e68572b14c131eb153ac03481
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-04-16 22:27:39 -0500

    feat: implemented footer content section

---
+++ src/components/news/components/footer-content.component.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "footer-content"
+}
+</script>
+
+<template>
+  <div>
+    <p>Copyright &copy; 2024 ALVARO CRISPIN DEV</p>
+  </div>
+  <div>
+    <p>Made with love using Vue</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
