e01e3c92ab7009f9b5c1c0f6c1e6f370d0c476d3
TheSeth0604 <alvarocrispin0604@gmail.com>
2024-04-16 21:35:59 -0500

    chore: imported components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
-
-
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import PrimeVue from "primevue/config";
+
+import 'primevue/resources/themes/md-light-indigo/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+
+import 'primeflex/primeflex.css';
+
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+import SelectButton from "primevue/selectbutton";
+import Sidebar from "primevue/sidebar";
+import Avatar from "primevue/avatar";
+import Menu from "primevue/menu";
+import Toolbar from "primevue/toolbar";
+import Menubar from "primevue/menubar";
+
+createApp(App)
+    .use(PrimeVue, { ripple: true })
+    .component('pv-card', Card)
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-sidebar', Sidebar)
+    .component('pv-avatar', Avatar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .component('pv-toolbar', Toolbar)
+    .mount('#app')
